d8ebd266e29ece7b3ea57ab9c4599833c3c5444e
AmgadSa <AmgadSa@outlook.com>
2022-12-07 17:44:28 +0300

    Add styles with necessary code adjusments

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+const $ = x=> document.querySelector(x);
 const container = document.querySelector(".container");
 const random = (n)=> Math.floor(Math.random()*(n+1));
 const pcCheck = document.querySelector("#PC");
@@ -23,15 +24,19 @@ const game = (()=>{
                 if(e.target===e.currentTarget){
                 mark(currentPlayer,e.target.index);
                 e.target.textContent = currentPlayer;}
-                });
+                },{once:true});
             grid.appendChild(box);
         };
+        container.innerHTML = `<p class="turn">Player ${currentPlayer}'s Turn</p>`;
         container.appendChild(grid);
     return 'Initialized'}
 
+    const updateTurn = () => $('.turn').textContent = `Player ${currentPlayer}'s Turn`;
+    
     const mark = (player,index)=>{
         board[index] = board[index] || player;
-        currentPlayer = ['x','o'].find(x=>x!==player);
+        currentPlayer = ['X','O'].find(x=>x!==player);
+        updateTurn();
         if (pc){playPC()};
         checkWinner();
         return 'Marked';
@@ -43,10 +48,11 @@ const game = (()=>{
             board[availableMoves[random(list.length-1)]] = currentPlayer;
         }
         currentPlayer = ['x','o'].find(x=>x!==currentPlayer);
+        updateTurn();
         return "PC played";
     }
     const checkWinner = ()=>{
-        let matrix = [board.slice(0,3).join('z'),board.slice(3,6).join('z'),board.slice(6,9).join('z')].toString();
+        let matrix = [board.slice(0,3).join(''),board.slice(3,6).join(''),board.slice(6,9).join('')].toString();
         let pattern = /(x|o)\1\1|^(x|o)\w+,\1\w+,\1\w+|\w+(x|o),\w+\1,\w+\1$|\w(o|x)\w,\w\1\w,\w\1\w|^(x|o)\w+,\w\1\w,\w+\1$|\w+(x|o),\w\1\w,\1\w+/ig;
         matrix = matrix.match(pattern);
         if(matrix){end(matrix[0].replace(pattern,"$1"))};
@@ -57,34 +63,45 @@ const game = (()=>{
         initialize();
         player1=player;
         currentPlayer = player1;
+        return 'Game started';
     }
     const end = winner => {
         score[winner]++
         let ResultScreen = document.createElement('div');
         ResultScreen.className = "result";
-        ResultScreen.innerHTML = `<p><span>${winner}</span> is Winner.<br>
+        ResultScreen.innerHTML = `<p><span>${winner}</span> is Winner<br><br>
                                     Results:<br> 
                                     ${score['x']>=score['o']?
                                     ('X : '+score['x']+'<br>O : '+score['o']):
                                     ("O : "+score['o']+'<br>X : '+score['x'])}
                                     <br>
-                                    <button onclick="game.start()">Play Again</button>`;
+                                    <div class="button" onclick="game.start()">Play Again</div>`;
         container.innerHTML = '';
         container.appendChild(ResultScreen);
+        return 'Game ended';
     }
     const draw = () => {
         let ResultScreen = document.createElement('div');
         ResultScreen.className = "result";
-        ResultScreen.innerHTML = `<p>Draw! Board full<br>
+        ResultScreen.innerHTML = `<p>Draw! Board full<br><br>
                                      Out of Moves<br>
                                     Results:<br> 
                                     ${score['x']>=score['o']?
                                     ('X : '+score['x']+'<br>O : '+score['o']):
                                     ("O : "+score['o']+'<br>X : '+score['x'])}
                                     <br>
-                                    <button onclick="game.start()">Play Again</button>`;
+                                    <div class="button" onclick="game.start()">restart</div>`;
         container.innerHTML = '';
         container.appendChild(ResultScreen);
+        return 'Draw';
     }
     return {start,pc}
+})();
+
+/* Style manuplation */
+
+const root = $(':root');
+((c = random(255))=>{
+    root.style.setProperty('--main-color',`hsl(${c},45%,80%)`);
+    root.style.setProperty('--main-background',`hsl(${c},45%,60%)`);
 })();
